f213c71491600b7b1b8a47fe9c8e6b59fe352118
Emma Claire Philippides <acme9898@gmail.com>
2017-03-07 13:22:00 -0800

    driver spec outline complete

--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -9,48 +9,40 @@ describe "RideShare::Driver" do
 
   describe "Driver#initialize" do
 
-    it "generates new instance of driver" do
-
-    end
-
     it "Takes a name, ID and VIN" do
-
     end
 
     it "raises a runtime error if the VIN is not the 17 characters in length" do
-
     end
+
   end
 
+  describe "self.find_all" do
 
-  describe "trip_list" do
+    it "retrieves a list of all drivers from the csv" do
+    end
+
+  end
+
+  describe "self.find(param) " do
+
+    it "looks up a driver using their id" do
+    end
+
+  end
+
+  describe "trip_list(param)" do
 
     it "outputs a list of all the driver's trips" do
     end
 
   end
 
-  describe "find.all" do
-
-    it "retrieves a list of all drivers from the csv" do
-    end
-  end
-
   describe "average_rating" do
 
     it "returns the average rating of driver" do
-
     end
 
   end
-
-  describe "find.driver(id) " do
-    it "looks up a driver using id" do
-
-    end
-
-  end
-
-
 
 end
